553e076f312b1c873fc81dbf5a21018fc08ad8de
thangnguyen1992 <ntthang4@cmcglobal.vn>
2025-06-11 16:26:15 +0700

    trtrtrt

---
+++ src/main/java/com/regain/accountservicemaven/config/MethodRestConfig.java
@@ -0,0 +1,29 @@
+package com.regain.accountservicemaven.config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MethodRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        // expose ids
+        // Cho phép trả về id
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+
+        // CORS configuration
+        String url = "http://localhost:3000";
+        cors.addMapping("/**")
+                .allowedOrigins(url)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
